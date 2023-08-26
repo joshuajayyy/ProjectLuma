20cd0bdbb58e4474af806ce121a469b5b39dcbc4
Joshua Jay <joshuajay.guadalupe@gmail.com>
2023-08-27 01:47:20 +0800

    Adding comments on Process an Order

--- test/test_processanorder.py
+++ test/test_processanorder.py
@@ -13,20 +13,26 @@ class TestProcessAnOrder(BaseClass):
     @pytest.mark.usefixtures("getData", "getProductInfo")
     def test_processAnOrder(self, getData, getProductInfo):
         log = self.getlogger()
+
+        # User Sign In
         signIn = LoginPage(self.driver)
         signIn.clickLoginButton().click()
 
+        # Enter username and password
         signIn.setEmail().send_keys(InputData().getUserEmail())
         signIn.setPassword().send_keys(getData["password"])
         signIn.clickLoginButton().click()
 
+        # Click on whats new page
         headerLink = HeaderLinks(self.driver)
         headerLink.clickWhatsNew().click()
 
+        # Navigate to whats new page and add product
         self.explicitWait(WhatsNewPage.target_element)
         whats_new = WhatsNewPage(self.driver)
         whats_new.productSearch(getProductInfo["product_name"])
 
+        # Set all necessary information from product page
         product_page = ProductPage(self.driver)
 
         product_page.setSize(getProductInfo["size"])
@@ -40,8 +46,10 @@ class TestProcessAnOrder(BaseClass):
             getProductInfo["product_name"], product_page.getSKU()))
         """
 
+        # Click on header cart icon
         self.explicitWait(headerLink.wait)
         headerLink.cartIcon().click()
 
+        # Click on header checkout button
         self.explicitWait(headerLink.wait2)
         headerLink.proceedToCheckOut().click()
