8b684979e0ddeb0d8627b11b4175c52871764b7a
Joshua Jay <joshuajay.guadalupe@gmail.com>
2023-08-28 08:38:00 +0800

    Fixed file naming conventions

--- testData/input_data.py
+++ testData/input_data.py
@@ -4,24 +4,31 @@ import randominfo as ri
 
 userLogFilePath = "C:/Users/SLMP - Joshua/PycharmProjects/ProjectLuma/testData/user_log.text"
 random_name = names.get_first_name()
-fname = ri.get_first_name()
-lname = ri.get_last_name()
-#email = ri.get_email(ri.Person().set_attr(ri.get_first_name()))
-pword = ri.random_password()
+f_name = ri.get_first_name()
+l_name = ri.get_last_name()
+#email = ri.get_email()
+p_word = ri.random_password()
 phone_number = ri.get_phone_number()
 
 class InputData:
 
-    create_account_data = [
-        {"firstname": fname, "lastname": lname, "email": random_name+"@gmail.com", "password": pword,
-         "company": "Case Closed Company", "phone": phone_number}
-    ]
+    create_account_data = [{
+        "firstname": f_name,
+        "lastname": l_name,
+        "email": random_name+"@gmail.com",
+        "password": p_word,
+        "company": "Case Closed Company",
+        "phone": phone_number
+    }]
 
-    selected_product = [
-        {"product_name": "Stellar Solar Jacket", "size": "S", "quantity": 3}
-    ]
+    selected_product = [{
+        "product_name": "Stellar Solar Jacket",
+        "size": "S",
+        "quantity": 3
+    }]
 
-    #{'address1': '37600 Sycamore Street', 'address2': '', 'city': 'Newark', 'state': 'CA', 'postalCode': '94560', 'coordinates': {'lat': 37.5261943, 'lng': -122.0304698}}
+    # 'address1': '37600 Sycamore Street', 'address2': '', 'city': 'Newark', 'state': 'CA', 'postalCode': '94560',
+    # 'coordinates': {'lat': 37.5261943, 'lng': -122.0304698}}
     random_name_address = [random_address.real_random_address()]
 
     states = [{
@@ -86,10 +93,10 @@ class InputData:
 
     def logData(self):
         with open(userLogFilePath, 'w') as writer:
-            writer.write(fname + "\n")
-            writer.write(lname + "\n")
+            writer.write(f_name + "\n")
+            writer.write(l_name + "\n")
             writer.write(random_name+"@gmail.com" + "\n")
-            writer.write(pword + "\n")
+            writer.write(p_word + "\n")
             writer.write(phone_number + "\n")
 
     def getUserFirstName(self):
@@ -121,5 +128,3 @@ class InputData:
             reader.readline()
             reader.readline()
             return reader.readline().strip()
-
-
